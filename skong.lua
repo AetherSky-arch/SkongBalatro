79a31e32bc8f5b432d5b6c044122f3e3e96ecce0
Aether Guez <theaethersky@gmail.com>
2025-03-12 21:04:21 +0100

    fix: add textures atlas

--- skong.lua
+++ skong.lua
@@ -1,3 +1,14 @@
+SMODS.Atlas {
+	-- Key for code to find it with
+	key = "skong",
+	-- The name of the file, for the code to pull the atlas from
+	path = "skong.png",
+	-- Width of each sprite in 1x size
+	px = 70,
+	-- Height of each sprite in 1x size
+	py = 95
+}
+
 SMODS.Joker {
 	key = 'hornet',
 	loc_txt = {
@@ -10,7 +21,7 @@ SMODS.Joker {
 		}
 	},
 	rarity = 3,
-	atlas = 'skong.png',
+	atlas = 'skong',
 	pos = { x = 0, y = 0 },
 	cost = 10,
 	calculate = function(self, card, context)
