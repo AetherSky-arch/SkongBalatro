e6d5db3f021de9d2fa3f253fbbb0b4774d3e98e5
Aether Guez <theaethersky@gmail.com>
2025-03-14 14:00:21 +0100

    fix: fix card mult adding logic

--- skong.lua
+++ skong.lua
@@ -20,14 +20,20 @@ SMODS.Joker {
             "{C:inactive}(Currently {X:mult,C:white}x6{} {C:inactive}Mult)",
 		}
 	},
+	config = { extra = { Xmult = 6 } },
 	rarity = 3,
 	atlas = 'skongatlas',
 	pos = { x = 0, y = 0 },
 	cost = 10,
+    loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.Xmult } }
+	end,
+
 	calculate = function(self, card, context)
 		if context.joker_main then
 			return {
-				Xmult_mod = 6,
+				message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } },
+				Xmult_mod = card.ability.extra.Xmult
 			}
 		end
 	end
