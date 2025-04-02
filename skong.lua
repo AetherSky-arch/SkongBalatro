f818c1bcec3775a75023a021098d8069f49b3f9e
Aether Guez <theaethersky@gmail.com>
2025-04-02 20:52:52 +0200

    feat: add hopium joker

--- skong.lua
+++ skong.lua
@@ -10,7 +10,7 @@ SMODS.Atlas {
 }
 
 SMODS.Joker {
-	key = 'hornet',
+	key = 'copium',
 	loc_txt = {
 		name = 'Silksong',
 		text = {
@@ -33,6 +33,69 @@ SMODS.Joker {
 				message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } },
 				Xmult_mod = card.ability.extra.Xmult
 			}
+		end
+	end
+}
+
+SMODS.Joker {
+	key = 'hopium',
+	loc_txt = {
+		name = 'Skong',
+		text = {
+			"{C:attention}In the end, Hopium prevails.{}",
+      "{X:mult,C:white}x10{} Mult",
+			"{C:green}1 in 4{} chance this card is",
+			"destroyed at end of round"
+		}
+	},
+	config = { extra = { Xmult = 10, odds = 4 } },
+	rarity = 4,
+	atlas = 'skongatlas',
+	pos = { x = 1, y = 0 },
+  soul_pos = { x = 2, y = 0 },
+	cost = 20,
+	eternal_compat = false,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.Xmult, (G.GAME.probabilities.normal or 1), card.ability.extra.odds } }
+	end,
+	calculate = function(self, card, context)
+		if context.joker_main then
+			return {
+				message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } },
+				Xmult_mod = card.ability.extra.Xmult
+			}
+		end
+		if context.end_of_round and context.game_over == false and not context.repetition and not context.blueprint then
+			if pseudorandom('hopium') < G.GAME.probabilities.normal / card.ability.extra.odds then
+				G.E_MANAGER:add_event(Event({
+					func = function()
+						play_sound('tarot1')
+						card.T.r = -0.2
+						card:juice_up(0.3, 0.4)
+						card.states.drag.is = true
+						card.children.center.pinch.x = true
+						G.E_MANAGER:add_event(Event({
+							trigger = 'after',
+							delay = 0.3,
+							blockable = false,
+							func = function()
+								G.jokers:remove_card(card)
+								card:remove()
+								card = nil
+								return true;
+							end
+						}))
+						return true
+					end
+				}))
+				return {
+					message = 'Doubted...'
+				}
+			else
+				return {
+					message = 'Keeps Hoping!'
+				}
+			end
 		end
 	end
 }
